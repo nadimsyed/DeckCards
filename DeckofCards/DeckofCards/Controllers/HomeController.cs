c2dd9e3701fa84dab814f54c443d5fe6087d16b3
Nadim <syed.m.nadim@gmail.com>
2018-06-07 00:45:40 -0400

    Did the extended challenge and made re-drawing 5 cards till deck is out work.  If randomly stops working again, follow comment i put above where input is placed in Index Action

--- DeckofCards/DeckofCards/Controllers/HomeController.cs
+++ DeckofCards/DeckofCards/Controllers/HomeController.cs
@@ -11,16 +11,18 @@ namespace DeckofCards.Controllers
 {
     public class HomeController : Controller
     {
+        string url = "";
+        string id = "";
         public ActionResult Index()
         {
             HttpWebRequest WR = WebRequest.CreateHttp("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
             WR.UserAgent = ".NET Framework Test Client";
 
-            HttpWebResponse Response;
+            HttpWebResponse Response3;
 
             try
             {
-                Response = (HttpWebResponse)WR.GetResponse();
+                Response3 = (HttpWebResponse)WR.GetResponse();
             }
             catch (WebException e)
             {
@@ -29,20 +31,103 @@ namespace DeckofCards.Controllers
                 return View();
             }
 
-            if (Response.StatusCode != HttpStatusCode.OK)
+            if (Response3.StatusCode != HttpStatusCode.OK)
             {
-                ViewBag.Error = Response.StatusCode;
-                ViewBag.ErrorDescription = Response.StatusDescription;
+                ViewBag.Error = Response3.StatusCode;
+                ViewBag.ErrorDescription = Response3.StatusDescription;
                 return View();
             }
 
-            StreamReader reader = new StreamReader(Response.GetResponseStream());
+            StreamReader reader = new StreamReader(Response3.GetResponseStream());
             string CardData = reader.ReadToEnd();
 
             try
             {
                 JObject JsonData = JObject.Parse(CardData);
-                ViewBag.Deck = JsonData["deck_id"];
+                ViewBag.DeckId = JsonData["deck_id"];
+                //Session["x"] = ViewBag.DeckId;
+                //string sess = (string)Session["x"];
+                //id = (string)JsonData["deck_id"];
+
+                //var domain = "https://deckofcardsapi.com/api/deck/";
+                //string id = ViewBag.DeckId;
+                //var end = "/draw /? count = 5";
+                //string full = $"{domain}{id}{end}";
+                //$"{domain}{ViewBag.DeckId}{end}"
+
+                //HttpCookie myCookie = new HttpCookie("url");
+                id = ViewBag.DeckId;
+                url = $"https://deckofcardsapi.com/api/deck/{ViewBag.DeckId}/draw/?count=5";
+                Response.Cookies["Link"].Value = url;
+                //myCookie.Expires = DateTime.Now.AddHours(12);
+                //HttpResponse.AppendCookie(myCookie);
+
+                var input = Request.Cookies["Link"] != null ? Request.Cookies["Link"].Value : String.Empty;
+
+                //if for some reason it starts throwing null exception reference again, just paste in url above and replace input and it will work again
+                HttpWebRequest WR2 = WebRequest.CreateHttp($"{input}");
+                WR2.UserAgent = ".NET Framework Test Client";
+
+                HttpWebResponse Response2;
+
+                try
+                {
+                    Response2 = (HttpWebResponse)WR2.GetResponse();
+                }
+                catch (WebException ex)
+                {
+                    ViewBag.Error2 = "Exception";
+                    ViewBag.ErrorDescription2 = ex.Message;
+                    return View();
+                }
+
+                if (Response2.StatusCode != HttpStatusCode.OK)
+                {
+                    ViewBag.Error2 = Response2.StatusCode;
+                    ViewBag.ErrorDescription2 = Response2.StatusDescription;
+                    return View();
+                }
+
+                StreamReader reader2 = new StreamReader(Response2.GetResponseStream());
+                string Card2Data = reader2.ReadToEnd();
+
+                try
+                {
+                    JObject Json2Data = JObject.Parse(Card2Data);
+                    ViewBag.Cards = /*(JObject)*/Json2Data["cards"];
+
+                    //ViewBag.Cards1 = (JObject)Json2Data["cards"][0];
+                    //ViewBag.Cards2 = (JObject)Json2Data["cards"][1];
+                    //ViewBag.Cards3 = (JObject)Json2Data["cards"][2];
+                    //ViewBag.Cards4 = (JObject)Json2Data["cards"][3];
+                    //ViewBag.Cards5 = (JObject)Json2Data["cards"][4];
+
+                    //ViewBag.Images1 = JsonData["cards"][0]["image"];
+                    //ViewBag.Images2 = JsonData["cards"][1]["image"];
+                    //ViewBag.Images3 = JsonData["cards"][2]["image"];
+                    //ViewBag.Images4 = JsonData["cards"][3]["image"];
+                    //ViewBag.Images5 = JsonData["cards"][4]["image"];
+
+                    //ViewBag.Value1 = JsonData["cards"][0]["value"];
+                    //ViewBag.Value2 = JsonData["cards"][1]["value"];
+                    //ViewBag.Value3 = JsonData["cards"][2]["value"];
+                    //ViewBag.Value4 = JsonData["cards"][3]["value"];
+                    //ViewBag.Value5 = JsonData["cards"][4]["value"];
+
+                    //ViewBag.Suit1 = JsonData["cards"][0]["suit"];
+                    //ViewBag.Suit2 = JsonData["cards"][1]["suit"];
+                    //ViewBag.Suit3 = JsonData["cards"][2]["suit"];
+                    //ViewBag.Suit4 = JsonData["cards"][3]["suit"];
+                    //ViewBag.Suit5 = JsonData["cards"][4]["suit"];
+
+
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Error2 = "JSON Issue";
+                    ViewBag.ErrorDescription2 = ex.Message;
+                    return View();
+                }
 
             }
             catch (Exception e)
@@ -54,13 +139,52 @@ namespace DeckofCards.Controllers
 
             return View();
         }
-
-        public ActionResult About()
+            
+        public ActionResult Draw()
         {
-            ViewBag.Message = "Your application description page.";
+            var input = Request.Cookies["Link"] != null ? Request.Cookies["Link"].Value : String.Empty;
 
-            return View();
+            HttpWebRequest WR2 = WebRequest.CreateHttp($"{input}");
+            WR2.UserAgent = ".NET Framework Test Client";
+
+            HttpWebResponse Response2;
+
+            try
+            {
+                Response2 = (HttpWebResponse)WR2.GetResponse();
+            }
+            catch (WebException ex)
+            {
+                ViewBag.Error2 = "Exception";
+                ViewBag.ErrorDescription2 = ex.Message;
+                return View("Index");
+            }
+
+            if (Response2.StatusCode != HttpStatusCode.OK)
+            {
+                ViewBag.Error2 = Response2.StatusCode;
+                ViewBag.ErrorDescription2 = Response2.StatusDescription;
+                return View("Index");
+            }
+
+            StreamReader reader2 = new StreamReader(Response2.GetResponseStream());
+            string Card2Data = reader2.ReadToEnd();
+
+            try
+            {
+                JObject Json2Data = JObject.Parse(Card2Data);
+                ViewBag.Cards = /*(JObject)*/Json2Data["cards"];
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error2 = "JSON Issue";
+                ViewBag.ErrorDescription2 = ex.Message;
+                return View("Index");
+            }
+
+            return View("Index");
         }
+        
 
         public ActionResult Contact()
         {
